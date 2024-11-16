79be05b5ec0b07e180bff1c9bef008e96ea59595
J-Chan91 <dev.jchan91@gmail.com>
2024-11-16 20:16:17 +0900

    Revert "feature: 단위 테스트 추가 및 암호화 라이브러리 변경 추가"
    This reverts commit 1a28e305b269d15b6b5a27f2ebf9e0a5f000210b.

--- app/(auth)/_action/createUser.ts
+++ app/(auth)/_action/createUser.ts
@@ -1,57 +1,50 @@
 "use server";
 
-import { Prisma } from "@prisma/client";
-import bcrypt from "bcrypt";
-
 import prisma from "@/lib/prisma";
-import { pattern } from "@/lib/pattern";
-
-const SALT_ROUNDS = 10;
-const ERROR_MESSAGES = {
-  INVALID_ID: "유효한 아이디가 아닙니다",
-  INVALID_PASSWORD: "유효한 비밀번호가 아닙니다",
-  DUPLICATE_ID: "이미 등록된 아이디입니다",
-};
+import { Prisma } from "@prisma/client";
+import CryptoJS from "crypto-js";
 
 export async function createUser(prevState: any, formData: FormData) {
   const userId = formData.get("userId") as string;
   const userPassword = formData.get("userPassword") as string;
 
-  if (!pattern.id.test(userId)) {
-    return { target: "userId", message: ERROR_MESSAGES.INVALID_ID };
+  if (!userId) {
+    return {
+      target: "userId",
+      message: "아이디를 입력해주세요",
+    };
   }
 
-  if (!pattern.password.test(userPassword)) {
-    return { message: ERROR_MESSAGES.INVALID_PASSWORD };
+  if (!userPassword) {
+    return {
+      target: "userPassword",
+      message: "비밀번호를 입력해주세요",
+    };
   }
-
-  const hashedPassword = await bcrypt.hash(userPassword, SALT_ROUNDS);
 
   try {
     const newUser = await prisma.user.create({
       data: {
         name: userId,
-        password: hashedPassword,
+        password: CryptoJS.SHA256(userPassword).toString(),
       },
     });
 
     console.log(newUser);
-
-    return {
-      target: "",
-      message: "",
-    };
   } catch (err) {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
       if (err.code === "P2002") {
         return {
+          target: "userId",
           message: "이미 등록된 아이디입니다",
         };
       }
-
-      return { message: "사용자 생성 중 오류가 발생했습니다" };
     }
   }
 
-  return { message: "사용자 생성 중 오류가 발생했습니다" };
+  return {
+    target: "",
+    message: "",
+  };
+  //   return redirect("/login");
 }
--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -6,13 +6,12 @@ import { Button } from "@/components/ui/button";
 import { createUser } from "@/app/(auth)/_action/createUser";
 
 const initState = {
+  target: "",
   message: "",
 };
 
 export default function Home() {
   const [state, formAction] = useFormState(createUser, initState);
-
-  console.log(state);
 
   return (
     <main className="flex h-full items-center justify-center">
@@ -56,7 +55,7 @@ export default function Home() {
             </label>
 
             <div className="h-6">
-              {state.message && (
+              {state.target && (
                 <span className="text-sm text-red-500">{state.message}</span>
               )}
             </div>
