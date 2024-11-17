271d3d94900a098f0da58e1e1a07e1dedbf954c1
J-Chan91 <95342579+J-Chan91@users.noreply.github.com>
2024-11-17 13:40:11 +0900

    feature: 회원가입 화면 및 기능 적용 (#1)
    * feature: 회원가입 페이지 작성
    
    * feature: 회원가입 액션 추가
    
    * refactor: 불필요한 기능 제거
    
    * feature: prisma 추가
    
    * change: 사용자 생성 action 수정
    
    * feature: 단위 테스트 추가 및 암호화 라이브러리 변경 추가
    
    * Revert "feature: 단위 테스트 추가 및 암호화 라이브러리 변경 추가"
    
    This reverts commit 1a28e305b269d15b6b5a27f2ebf9e0a5f000210b.
    
    * change: 라이브러리 수정
    
    - 테스트 스크립트 추가
    - 암호화 라이브러리 추가
    
    * feature: 입력 단위 테스트 추가
    
    * change: 액션 기능 리팩토링

---
+++ app/(auth)/_action/createUser.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/prisma";
+import { pattern } from "@/lib/pattern";
+
+const SALT_ROUNDS = 10;
+const ERROR_MESSAGES = {
+  INVALID_ID: "유효한 아이디가 아닙니다",
+  INVALID_PASSWORD: "유효한 비밀번호가 아닙니다",
+  DUPLICATE_ID: "이미 등록된 아이디입니다",
+};
+
+export async function createUser(prevState: any, formData: FormData) {
+  const userId = formData.get("userId") as string;
+  const userPassword = formData.get("userPassword") as string;
+
+  if (!pattern.id.test(userId)) {
+    return { message: ERROR_MESSAGES.INVALID_ID };
+  }
+
+  if (!pattern.password.test(userPassword)) {
+    return { message: ERROR_MESSAGES.INVALID_PASSWORD };
+  }
+
+  const hashedPassword = await bcrypt.hash(userPassword, SALT_ROUNDS);
+
+  try {
+    await prisma.user.create({
+      data: {
+        name: userId,
+        password: hashedPassword,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === "P2002") {
+        return {
+          message: "이미 등록된 아이디입니다",
+        };
+      }
+
+      return { message: "사용자 생성 중 오류가 발생했습니다" };
+    }
+  }
+
+  redirect("login");
+}
